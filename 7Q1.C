df7339228584cc265563e31189098ac1693aa4ff
khairetanaya2005 <khairetanaya2005@gmail.com>
2026-05-29 00:27:07 +0530

    Add files via upload

---
+++ 7Q1.C
@@ -0,0 +1,54 @@
+/*
+
+1.Write a program which accept number from user and display below pattern.
+
+Input : 5 
+Output : * * * * * # # # # #
+
+Input : 6
+Output : * * * * * * # # # # # # #
+
+Input : -5 
+Output : * * * * * # # # # #
+
+Input : 2
+Output : * * # #
+
+
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+   
+    for(int iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("* \t");
+    }
+
+  
+    for(int iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("# \t");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+    
+    return 0;
+}
