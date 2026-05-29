740c29780cae59a742f019891ad6a16d7e3fb90b
khairetanaya2005 <khairetanaya2005@gmail.com>
2026-05-29 11:14:19 +0530

    Add files via upload

---
+++ Assignment7/7Q2.C
@@ -0,0 +1,39 @@
+/*
+
+2. Accept amount in US dollar and return its corresponding value in Indian currency. 
+Consider 1$ as 70 rupees. 
+
+Input : 10
+Output : 700
+
+Input : 3
+Output : 210   (Note: assignment text says 270 by typo, but 3 * 70 = 210)
+
+Input : 1200
+Output : 84000
+
+
+*/
+
+#include<stdio.h>
+
+int DollarToINR(int iNo)
+{
+    
+
+    return iNo * 70;
+}
+
+int main()
+{
+
+    int iValue = 0, iRet = 0;
+    
+    printf("Enter number of USD: ");
+    scanf("%d",&iValue);
+    
+    iRet = DollarToINR(iValue);
+    printf("Value in INR is %d\n",iRet);
+    
+    return 0;
+}
