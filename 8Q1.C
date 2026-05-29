b41a9912e68a3e2b9430e377d03a420699c6c99e
khairetanaya2005 <khairetanaya2005@gmail.com>
2026-05-29 11:14:43 +0530

    Add files via upload

---
+++ 8Q1.C
@@ -0,0 +1,39 @@
+/*
+
+1.Write a program which accept radius of circle from user and calculate its area.
+Consider value of PI as 3.14. (Area = PI * Radius * Radius)
+
+Input : 5.3
+Output : 88.2026
+
+Input : 10.4
+Output : 339.6224
+
+*/
+#include<stdio.h>
+
+double CircleArea(float fRadius)
+{
+    float fPI = 3.14f;
+    double dArea = 0.0;
+    
+    
+    dArea = fPI * fRadius * fRadius;
+    
+    return dArea;
+}
+
+int main()
+{
+    float fValue = 0.0;
+    double dRet = 0.0;
+    
+    printf("Enter radius: ");
+    scanf("%f",&fValue);
+    
+    dRet = CircleArea(fValue);
+    
+    printf("Area of circle is: %lf\n", dRet);
+    
+    return 0;
+}
